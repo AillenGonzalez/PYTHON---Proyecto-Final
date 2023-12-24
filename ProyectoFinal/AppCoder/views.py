c31129dfb65a6723817ec908566aa68f110dc605
AillenGonzalez <ailleng@hotmail.com>
2023-12-23 21:01:15 -0300

    Servidor en funcionamiento

--- ProyectoFinal/AppCoder/views.py
+++ ProyectoFinal/AppCoder/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import Template, Context
 from datetime import datetime
+from .forms import CursoFormulario
 
 def inicio_view(request):
     return render(request, "AppCoder/inicio.html")
 
 def cursos_view(request):
     if request.method == "GET":
-        print("+" * 90)
-        print("+" * 90)
+        form = CursoFormulario()
         return render(
             request,
-            "AppCoder/curso_formulario_basico.html",
+            "AppCoder/curso_formulario_avanzado.html",
+            context={"form": form}
         )
     else:
-        print("*" * 90)
         print(request.POST)
-        print("*" * 90)
-        return render(
-            request,
-            "AppCoder/curso_formulario_basico.html"
-        )
+
+        from .models import Curso
+
+        mi_curso = Curso(curso=request.POST["curso"], camada=request.POST["camada"])
+        mi_curso.save()
+
+        return redirect("AppCoder:inicio")
 
 def profesores_view(request):
     return render(request, "AppCoder/padre.html")
